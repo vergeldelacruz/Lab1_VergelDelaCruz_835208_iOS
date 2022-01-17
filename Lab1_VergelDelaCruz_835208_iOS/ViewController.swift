4fa9eb49b3ea18a3fc841cc02b6298cdbce8c1a2
Vergel dela Cruz <C0835208@mylambton.ca>
2022-01-17 18:42:37 -0500

    Initial Commit

---
+++ Lab1_VergelDelaCruz_835208_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1_VergelDelaCruz_835208_iOS
+//
+//  Created by user209721 on 1/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
